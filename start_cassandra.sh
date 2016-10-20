2d01331f965ec22eeb31a0b22468262c60dd11d5
Danny Krainas <dkrainas87@gmail.com>
2016-10-20 16:44:06 -0400

    use official cassandra image

--- start_cassandra.sh
+++ start_cassandra.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo docker pull tobert/cassandra
+sudo docker pull cassandra:3.0
 mkdir -p $HOME/.cassandra-data
-sudo docker run -d -v $HOME/.cassandra-data:/data -p 9042:9042 tobert/cassandra
+sudo docker run -d -v $HOME/.cassandra-data:/var/lib/cassandra -p 9042:9042 cassandra:3.0
